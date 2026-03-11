e5036b43eaa16622642c07f26fe43743d4c38711
Bhavy Manchanda <bhavyb1234@gmail.com>
2026-03-11 14:44:42 +0530

    UC1: Add welcome message module

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,9 +1,21 @@
 /**
+ * UseCase1HotelBookingApp
+ *
+ * Entry point of the Hotel Booking Application.
+ * This program prints a welcome message along with
+ * the application name and version.
+ *
  * @author Bhavy Manchanda
+ * @version 1.0
  */
-public class BookMyStayApp {
-
+public class UseCase1HotelBookingApp {
     public static void main(String[] args) {
 
+        System.out.println("=================================");
+        System.out.println(" Welcome to Hotel Booking System ");
+        System.out.println(" Application Version : v1.0 ");
+        System.out.println("=================================");
+
+        System.out.println("Application started successfully.");
     }
 }
